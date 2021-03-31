ca2d1f08a47fb24490d692d11305efe666a38609
Thirumalai Aiyalu <thiru85@gmail.com>
2021-03-31 10:02:34 +0800

    committing to purgatory. No, just git

---
+++ ipc_greengrass.py
@@ -0,0 +1,39 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+# Author: Thirumalai Aiyalu (taiyalu@amazon.com)
+# Modified from AWS IoT SDK v2 for Python
+
+import json
+import time
+import os
+import random
+
+import awsiot.greengrasscoreipc
+import awsiot.greengrasscoreipc.model as model
+
+if __name__ == '__main__':
+    ipc_client = awsiot.greengrasscoreipc.connect()
+    
+    while True:
+        telemetry_data = {
+            "timestamp": int(round(time.time() * 1000)),
+            "battery_state_of_charge": random.randint(40,50),
+            "location": {
+                "longitude": random.uniform(45,60),
+                "latitude": random.uniform(45,70),
+            },
+        }
+
+        op = ipc_client.new_publish_to_iot_core()
+        op.activate(model.PublishToIoTCoreRequest(
+            topic_name="my/iot/{}/telemetry".format(os.getenv("AWS_IOT_THING_NAME")),
+            qos=model.QOS.AT_LEAST_ONCE,
+            payload=json.dumps(telemetry_data).encode(),
+        ))
+        try:
+            result = op.get_response().result(timeout=5.0)
+            print("successfully published message:", result)
+        except Exception as e:
+            print("failed to publish message:", e)
+
+        time.sleep(3)
